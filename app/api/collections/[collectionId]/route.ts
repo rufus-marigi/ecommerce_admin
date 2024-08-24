170e5244a5235184e963a5f2a3993c9a354a4e2c
rufus-marigi <rufusmarigik@gmail.com>
2024-08-25 01:13:27 +0300

    Create route.ts

---
+++ app/api/collections/[collectionId]/route.ts
@@ -0,0 +1,83 @@
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+import { connectToDB } from "@/lib/mongoDB";
+import Collection from "@/lib/models/Collection";
+
+export const GET = async (
+  req: NextRequest,
+  { params }: { params: { collectionId: string } }
+) => {
+  try {
+    await connectToDB();
+    const collection = await Collection.findById(params.collectionId);
+    if (!collection) {
+      return new NextResponse(
+        JSON.stringify({ message: "Collection not found" }),
+        { status: 404 }
+      );
+    }
+    return NextResponse.json(collection, { status: 200 });
+  } catch (err) {
+    console.log("[collectionId_GET]", err);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+};
+
+export const POST = async (
+  req: NextRequest,
+  { params }: { params: { collectionId: string } }
+) => {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+    await connectToDB();
+    let collection = await Collection.findById(params.collectionId);
+    if (!collection) {
+      return new NextResponse("Collection not found", { status: 404 });
+    }
+
+    const { title, description, image } = await req.json();
+
+    if (!title || !image) {
+      return new NextResponse("Title and image are required", { status: 400 });
+    }
+
+    collection = await Collection.findByIdAndUpdate(
+      params.collectionId,
+      { title, description, image },
+      { new: true }
+    );
+
+    await collection.save();
+
+    return NextResponse.json(collection, { status: 200 });
+  } catch (err) {
+    console.log("[collectionId_POST]", err);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+};
+
+export const DELETE = async (
+  req: NextRequest,
+  { params }: { params: { collectionId: string } }
+) => {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    await connectToDB();
+
+    await Collection.findByIdAndDelete(params.collectionId);
+    return new NextResponse("Collection deleted", {
+      status: 200,
+    });
+  } catch (err) {
+    console.log("[collectionId_DELETE]", err);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+};
